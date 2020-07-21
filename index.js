c187ec430f9fff932b72aff9a0e25f95edb3a612
Olav Fosse <fosseolav@gmail.com>
2020-07-22 00:56:58 +0200

    Only allow sending payloads onopen

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ const url = process.argv[2]
 const WebSocket = new ws(url)
 const stdin = process.openStdin()
 
-if(process.argv[3]){
-  WebSocket.onopen = () => WebSocket.send(process.argv[3])
-}
-
-stdin.addListener("data", async (data) => {
-  console.log('SENT---------------------------------------------------------------------------')
-  const input = data.toString().trim()
-  WebSocket.send(input)
-})
-
-WebSocket.onmessage = ({data}) => {
-  console.log(JSON.parse(data))
-  console.log('RECEIVED-----------------------------------------------------------------------')
-}
-
-WebSocket.onclose = ({data}) => {
-  console.error('Connection closed')
-  process.exit()
+WebSocket.onopen = () => {
+  if(process.argv[3]){
+    WebSocket.send(process.argv[3])
+ }
+ 
+ stdin.addListener("data", async (data) => {
+   const input = data.toString().trim()
+   WebSocket.send(input)
+   console.log('SENT---------------------------------------------------------------------------')
+ })
+ 
+ WebSocket.onmessage = ({data}) => {
+   console.log(JSON.parse(data))
+   console.log('RECEIVED-----------------------------------------------------------------------')
+ }
+ 
+ WebSocket.onclose = () => {
+   console.error('Connection closed')
+   process.exit()
+ }
 }
